ab4315d56e09e8d65b629c6f0862599aae7fc88f
obrusvit <vit.obrusnik@gmail.com>
2019-07-09 22:29:01 +0200

    rename file

--- gui/drill_12.cpp
+++ gui/drill_12.cpp
@@ -60,48 +60,6 @@ int main(){
         win.attach(se3);
         win.attach(se4);
 
-        //Point center(win.x_max() / 2, win.y_max() / 2);
-        //Graph_lib::Open_polyline supEllipse0;
-        //supEllipse0.set_color(Color::red);
-        //Graph_lib::Open_polyline supEllipse1;
-        //supEllipse1.set_color(Color::green);
-        //Graph_lib::Open_polyline supEllipse2;
-        //supEllipse2.set_color(Color::blue);
-        //Graph_lib::Open_polyline supEllipse3;
-        //supEllipse3.set_color(Color::black);
-     
-        //double a = 100;
-        //double y;
-        //double b = 100;
-        //double n; // 1/2; 3/2; 2; 4;
-        //n = 2.0;
-     
-        //for (int x = -100; x < 0; x++) {
-        //    y = b * pow(1 - pow(abs(x / a), n), 1 / n);
-        //    supEllipse0.add(Point(x + center.x, y + center.y));
-        //}
-        //for (int x = 0; x < 101; x++) {
-        //    y = b * pow(1 - pow(abs(x / a), n), 1 / n);
-        //    supEllipse1.add(Point(x + center.x, y + center.y));
-        //}
-     
-        //for (int x = 100; x > 0; x--) {
-        //    y = b * pow(1 - pow(abs(x / a), n), 1 / n);
-        //    y = -y;
-        //    supEllipse2.add(Point(x + center.x, y + center.y));
-        //}
-     
-        //for (int x = 0; x > -101; x--) {
-        //    y = b * pow(1 - pow(abs(x / a), n), 1 / n);
-        //    y = -y;
-        //    supEllipse3.add(Point(x + center.x, y + center.y));
-        //}
-     
-        //win.attach(supEllipse0);
-        //win.attach(supEllipse1);
-        //win.attach(supEllipse2);
-        //win.attach(supEllipse3);       //win.attach(se4);
-        
         win.wait_for_button();
         return 0;
     }
